d22cecd44788207737288a2933c3a9ad08b25c4a
GitHub Action <action@github.com>
2023-08-16 05:09:34 +0000

    Apply Black formatting

--- autopep8-testing.py
+++ autopep8-testing.py
@@ -15,15 +15,12 @@ p = Tester()
 
 p.printer("Hello World!")
 """
+
+
 # Code after github workflow action
 class Tester(object):
-    
-    
-    
     def printer(self, message):
         print(message)
-
-
 
 
 p = Tester()
