332c9c68bc47abac52a3ce7400117fd0c8c20248
parijat0904114 <parijat2009@gmail.com>
2023-08-15 23:13:00 -0600

    Create autopep8-testing.py

---
+++ autopep8-testing.py
@@ -0,0 +1,29 @@
+# Original Code
+"""
+class Tester(object):
+    
+    
+    
+    def printer(self, message):
+        print(message)
+
+
+
+
+p = Tester()
+
+
+p.printer("Hello World!")
+"""
+
+
+# Code after github workflow action
+class Tester(object):
+    def printer(self, message):
+        print(message)
+
+
+p = Tester()
+
+
+p.printer("Hello World!")
