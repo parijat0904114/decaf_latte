f28f5fe52fee9795477aca3e15c1c5c1f77a99f7
GitHub Action <action@github.com>
2023-08-16 05:02:00 +0000

    Apply autopep8 formatting

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 # Original Code
 """
 class Tester(object):
-    
-    
-    
+
+
+
     def printer(self, message):
         print(message)
 
@@ -16,14 +16,12 @@ p = Tester()
 p.printer("Hello World!")
 """
 # Code after github workflow action
+
+
 class Tester(object):
-    
-    
-    
+
     def printer(self, message):
         print(message)
-
-
 
 
 p = Tester()
