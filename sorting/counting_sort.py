98edf1d539cfd4e460cab949eed2037bb42685ea
Parijat Purohit <parijat0904114@gmail.com>
2023-12-21 02:19:55 -0700

    counting sort

---
+++ sorting/counting_sort.py
@@ -0,0 +1,19 @@
+def counting_sort(arr):
+    counter = [0] * (max(arr) + 1)
+    for num in arr:
+        counter[num] += 1
+    out = []
+    for index, number in enumerate(counter):
+        if number > 0:
+            for _ in range(number):
+                out.append(index)
+    return out
+
+    # print(counting_sort([2, 1, 3, 4, 4, 2, 6, 5]))
+    """
+    Counting Sort is best when we deal with small numbers named as k.
+    Then the size of the counter would be maximum k.
+    So we can put the condition k<=n.
+    Time Complexity: O(n + k)
+    Space Complexity: O(k)
+    """
