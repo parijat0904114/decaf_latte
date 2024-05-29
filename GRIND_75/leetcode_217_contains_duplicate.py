d1b1914be37840f22cd23d3ab1cd7d2e33351c0a
Parijat Purohit <parijat0904114@gmail.com>
2024-05-29 00:24:44 -0600

    add two solutions

--- GRIND_75/leetcode_217_contains_duplicate.py
+++ GRIND_75/leetcode_217_contains_duplicate.py
@@ -1,4 +1,5 @@
-class Solution(object):
+# Better Time Complexity
+class Solution(object): 
     def containsDuplicate(self, nums):
         """
         :type nums: List[int]
@@ -11,6 +12,23 @@ class Solution(object):
             s.add(num)
         return False
 
+# Better Space Complexity    
+class Solution2(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Taking advantage of not using any extra space
+        # by Sorting.
+        nums = sorted(nums)
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
 
 # s = Solution()
 # print(s.containsDuplicate([1, 2, 3, 1]))
+
+# s = Solution2()
+# print(s.containsDuplicate([3,4,1,4,5,2]))
