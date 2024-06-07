3a98b7d34c9fb9ed9dab11eed49f9767eea184f3
Parijat Purohit <parijat0904114@gmail.com>
2024-06-07 12:12:31 -0600

    add improvement

--- GRIND_75_Practice_1/leetcode_121_best_time_to_buy_and_sell_stock.py
+++ GRIND_75_Practice_1/leetcode_121_best_time_to_buy_and_sell_stock.py
@@ -5,6 +5,22 @@ class Solution(object):
         :rtype: int
         """
         max_profit = 0
+        min_price = 10**4 + 1
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            if (price - min_price) > max_profit:
+                max_profit = price - min_price
+        return max_profit
+
+# Improved Version
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
         # can consider the first indexed price as min_price.
         min_price = prices[0]
         for i in range(1, len(prices)):
@@ -14,5 +30,5 @@ class Solution(object):
                 max_profit = prices[i] - min_price
         return max_profit
 
-# s = Solution()
-# s.maxProfit([7,1,5,3,6,4])
+s = Solution()
+print(s.maxProfit([7,1,5,3,6,4]))
