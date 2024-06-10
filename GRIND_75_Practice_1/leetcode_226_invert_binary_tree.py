4ca6daa7106290047fe001d46e4d0ad9e9f28e0f
Parijat Purohit <parijat0904114@gmail.com>
2024-06-09 23:24:17 -0600

    leetcode_226

---
+++ GRIND_75_Practice_1/leetcode_226_invert_binary_tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        root.left, root.right = root.right, root.left
+        if root.left:
+            self.invertTree(root.left)
+        if root.right:
+            self.invertTree(root.right)
+        return root
+
+
+# t1 = TreeNode(1)
+# t2 = TreeNode(2)
+# t3 = TreeNode(3)
+# t4 = TreeNode(4)
+# t5 = TreeNode(5)
+# t6 = TreeNode(6)
+# t1.left = t2
+# t2.left = t3
+# t2.right = t4
+# t4.left = t5
+# t4.right = t6
+
+# s = Solution()
+# print(s.invertTree(t1))
