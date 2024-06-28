57868e6b54bdccbbba842ddbd4813ccbcfc514c1
Parijat Purohit <parijat0904114@gmail.com>
2024-06-27 23:38:22 -0400

    leetcode 141

---
+++ GRIND_75_Practice_1/leetcode_141_linked_list_cycle.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None:
+            return False
+
+        slow_pointer = fast_pointer = head
+
+        while((fast_pointer.next and fast_pointer.next.next) is not None):
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if slow_pointer == fast_pointer:
+                return True
+        return False
+
+
+l1 = ListNode(1)
+l2 = ListNode(2)
+l3 = ListNode(3)
+
+l1.next = l2
+l2.next = l3
+
+s = Solution()
+print(s.hasCycle(l1))
