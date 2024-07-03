1fe2664cc6d00bbc7aa50cccddbc251e80c0235b
Parijat Purohit <parijat0904114@gmail.com>
2024-07-03 00:32:57 -0400

    update comment

--- GRIND_75_Practice_1/leetcode_278_first_bad_version.py
+++ GRIND_75_Practice_1/leetcode_278_first_bad_version.py
@@ -3,10 +3,6 @@
 # @return a bool
 # def isBadVersion(version):
 
-# The isBadVersion API is already defined for you.
-# @param version, an integer
-# @return a bool
-# def isBadVersion(version):
 
 class Solution(object):
     def firstBadVersion(self, n):
@@ -24,4 +20,9 @@ class Solution(object):
             else:
                 low = mid + 1
         return low
-            
+
+# Returning either low or high is sufficient to display the
+# correct answer. However, returning mid wouldn't work.
+# As you can see we increase the value of low by adding one with the
+# value of mid. So if a calcuated mid value is 3, low would have the value 4 and maybe
+# that's where the low converges with high.
