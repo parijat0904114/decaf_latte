5d5ecb4bf4485fc3942a6c57f7703da48ccd090a
Parijat Purohit <parijat0904114@gmail.com>
2024-07-09 23:07:02 -0400

    leetcode_70

---
+++ GRIND_75_Practice_1/leetcode_70_climbing_stairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # climbing stairs is just another form of fibonacci
+        # base case 1,2 and then 3,5,8,13,21...
+        a = 1
+        b = 2
+        while(n>1):
+            a, b = b, a+b
+            n-=1
+        return a
+
+# s = Solution()
+# print(s.climbStairs(5))
