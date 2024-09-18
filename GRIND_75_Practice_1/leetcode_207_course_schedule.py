2ffea14f67838eb068b443e9f6a86988349ffbcd
Parijat Purohit <parijat0904114@gmail.com>
2024-09-17 22:28:57 -0600

    leetcode 207_peped_8

--- GRIND_75_Practice_1/leetcode_207_course_schedule.py
+++ GRIND_75_Practice_1/leetcode_207_course_schedule.py
@@ -1,6 +1,8 @@
 # Topological Sort & BFS
 
 from collections import defaultdict, deque
+
+
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
@@ -9,30 +11,28 @@ class Solution(object):
         :rtype: bool
         """
         graph = defaultdict(list)
-
         in_degree = [0]*numCourses
 
         for course, prereq in prerequisites:
             graph[prereq].append(course)
             in_degree[course] += 1
 
-        course_queue = deque(course for course in range(numCourses) if in_degree[course] == 0)
-        print(course_queue)
+        course_queue = deque(course for course in range(
+            numCourses) if in_degree[course] == 0)
 
         courses_taken = 0
 
-        while(course_queue):
+        while (course_queue):
             current_course = course_queue.popleft()
             courses_taken += 1
 
             for neighbour in graph[current_course]:
-                in_degree[neighbour] -=1
+                in_degree[neighbour] -= 1
                 if in_degree[neighbour] == 0:
                     course_queue.append(neighbour)
 
-
         return courses_taken == numCourses
-            
+
 
 s = Solution()
-print(s.canFinish(2, [[1,0]]))
+print(s.canFinish(2, [[1, 0]]))
