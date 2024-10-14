5e6fcc7492fb4c574f03896be3b6e2da6316425b
Parijat Purohit <parijat0904114@gmail.com>
2024-10-13 23:33:01 -0400

    rotated binary array

---
+++ GRIND_75_Practice_1/leetcode_33_search_in_rotated_sorted_array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
+
+s = Solution()
+print(s.search([4, 5, 6, 7, 8, 1, 2, 3], 2))
