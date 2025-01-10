8b3f454c7625b8f112619fb1f9a3e1d42339a8ef
Parijat Purohit <parijat0904114@gmail.com>
2025-01-10 21:32:43 +0600

    approach in a different way

--- GRIND_75_Practice_1/leetcode_39_combination_sum.py
+++ GRIND_75_Practice_1/leetcode_39_combination_sum.py
@@ -1,3 +1,27 @@
+# class Solution(object):
+#     def combinationSum(self, candidates, target):
+#         """
+#         :type candidates: List[int]
+#         :type target: int
+#         :rtype: List[List[int]]
+#         """
+#         result = []
+
+#         def dfs(i, cur, total):
+#             if total == target:
+#                 result.append(list(cur))
+#                 return
+#             if total > target or i >= len(candidates):
+#                 return
+#             cur.append(candidates[i])
+#             dfs(i, cur, total + candidates[i])
+#             cur.pop()
+#             dfs(i+1, cur, total)
+
+#         dfs(0, [], 0)
+#         return result
+
+
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -5,22 +29,22 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        result = []
+        res = []
 
         def dfs(i, cur, total):
-            if total == target:
-                result.append(cur.copy())
+            if total == 0:
+                res.append(list(cur))
                 return
-            if total > target or i >= len(candidates):
+            if total < 0 or i < 0:
                 return
             cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
+            dfs(i, cur, total-candidates[i])
             cur.pop()
-            dfs(i+1, cur, total)
+            dfs(i-1, cur, total)
 
-        dfs(0, [], 0)
-        return result
+        dfs(len(candidates)-1, [], target)
+        return res
 
 
 s = Solution()
-print(s.combinationSum([2, 3, 4, 7], 7))
+print(s.combinationSum([2, 3, 4, 7], 4))
